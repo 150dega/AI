bab3e7ee42910503d55324d340684d403cf427b1
150dega <himanshu.dubey.pusulaint@gmail.com>
2026-04-23 20:44:14 +0530

    Update DFS.py

--- DFS.py
+++ DFS.py
@@ -1,20 +1,19 @@
-# Depth First Search (DFS) using Recursion
+
 
 def dfs(graph, node, visited):
 
-    # Check if node is already visited
+
     if node not in visited:
         print(node, end=" ")
 
-        # Mark node as visited
+
         visited.append(node)
 
-        # Visit all neighbors
+        
         for neighbor in graph[node]:
             dfs(graph, neighbor, visited)
 
 
-# Example Graph (Adjacency List)
 graph = {
     'A': ['B', 'C'],
     'B': ['D', 'E'],
